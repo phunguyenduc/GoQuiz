f93cfc6c6f28b306b4dc7ffc380d233cf14f6f43
Peg4sus <nvu18032003@gmail.com>
2023-12-01 21:47:13 +0700

    update

--- app/src/main/java/com/example/goquiz/Categories.java
+++ app/src/main/java/com/example/goquiz/Categories.java
@@ -8,23 +8,30 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.example.goquiz.databinding.ActivityCategoriesBinding;
+
 import java.util.ArrayList;
 
 public class Categories extends AppCompatActivity {
+
+    private ActivityCategoriesBinding binding;
     private ArrayList<CategoriesClass> categories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_categories);
+        binding = ActivityCategoriesBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
         ListView categoriesListView = findViewById(R.id.CategoriesListView);
 
         categories = new ArrayList<>();
         categories.add(new CategoriesClass("Địa lý", R.drawable.mongolia, "dia ly thoi"));
         categories.add(new CategoriesClass("Lịch sử", R.drawable.mongolia, "đưaăd"));
-        categories.add(new CategoriesClass("Khoa học", R.drawable.mongolia, "kahsod hco"));
-        categories.add(new CategoriesClass("Nghệ thuật", R.drawable.mongolia, "contrydsaide"));
+        categories.add(new CategoriesClass("Công nghệ", R.drawable.mongolia, "kahsod hco"));
+        categories.add(new CategoriesClass("Sinh học", R.drawable.mongolia, "contrydsaide"));
+        categories.add(new CategoriesClass("Thiên văn học", R.drawable.mongolia, "abc"));
 
         CategoriesAdapter adapter = new CategoriesAdapter(this, categories);
         categoriesListView.setAdapter(adapter);
@@ -38,6 +45,10 @@ public class Categories extends AppCompatActivity {
                 intent.putExtra("attraction", selected);
                 startActivity(intent);*/
             }
+        });
+        binding.QuestionsButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Categories.this, Questions.class);
+            startActivity(intent);
         });
     }
 }
